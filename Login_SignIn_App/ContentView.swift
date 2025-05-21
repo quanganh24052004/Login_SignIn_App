41a84611ab211c70f0077ea3b684eba7bd5dab6a
Nguyễn Quang Anh <anh.nguyenquang24052004@gmail.com>
2025-05-22 06:55:42 +0700

    Initial Commit

---
+++ Login_SignIn_App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Login_SignIn_App
+//
+//  Created by Nguyễn Quang Anh on 22/5/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Login_SignIn_App/Login_SignIn_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Login_SignIn_AppApp.swift
+//  Login_SignIn_App
+//
+//  Created by Nguyễn Quang Anh on 22/5/25.
+//
+
+import SwiftUI
+
+@main
+struct Login_SignIn_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
