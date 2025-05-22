6909e752858879aca9d640f000f5c2073c6f2e24
Nguyễn Quang Anh <anh.nguyenquang24052004@gmail.com>
2025-05-22 07:12:05 +0700

    Khởi tạo ban đầu

---
+++ Login_SignIn_App/Helpers/View+Extensions.swift
@@ -0,0 +1,18 @@
+//
+//  View+Extensions.swift
+//  Login_SignIn_App
+//
+//  Created by Nguyễn Quang Anh on 22/5/25.
+//
+
+import SwiftUI
+
+struct View_Extensions: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    View_Extensions()
+}
